4046d4afbcc1f569f421097fcd61b3d0b1ecd2b3
webyazilim0065 <vodata6742@d3bb.com>
2021-10-23 15:08:00 +0300

    Bubble sort algorithm integrated

--- CSharp_Diziler/Program.cs
+++ CSharp_Diziler/Program.cs
@@ -43,7 +43,7 @@ namespace CSharp_Diziler
             //sayilar[0] = "str"; hatalı atama  işlemi
             for (int i = 0; i < sayilar.Length; i++)
             {
-                Console.WriteLine($"{i+1}. sayıyı giriniz.");
+                Console.WriteLine($"{i + 1}. sayıyı giriniz.");
                 sayilar[i] = Convert.ToInt32(Console.ReadLine());
             }
 
@@ -52,39 +52,59 @@ namespace CSharp_Diziler
             //    Console.WriteLine(sayilar[j]);
             //}
 
-            int toplam = 0;
-            for (int i = 0; i < sayilar.Length; i++)
-            {
-                toplam += sayilar[i];
-            }
-            Console.WriteLine(toplam);
-            int enBuyuk = int.MinValue;
-            int enKucuk = int.MaxValue;
-            int enKucukIndex = 0;
-            int enBuyukIndex = 0;
-            for (int i = 0; i < sayilar.Length; i++)
-            {
-                if(sayilar[i] > enBuyuk)
-                {
-                    enBuyuk = sayilar[i];
-                    enBuyukIndex = i;
-                }
-                if(sayilar[i] < enKucuk)
-                {
-                    enKucuk = sayilar[i];
-                    enKucukIndex = i;
-                }
-            }
+            //int toplam = 0;
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    toplam += sayilar[i];
+            //}
+            //Console.WriteLine(toplam);
+            //int enBuyuk = int.MinValue;
+            //int enKucuk = int.MaxValue;
+            //int enKucukIndex = 0;
+            //int enBuyukIndex = 0;
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    if(sayilar[i] > enBuyuk)
+            //    {
+            //        enBuyuk = sayilar[i];
+            //        enBuyukIndex = i;
+            //    }
+            //    if(sayilar[i] < enKucuk)
+            //    {
+            //        enKucuk = sayilar[i];
+            //        enKucukIndex = i;
+            //    }
+            //}
 
-            int temp = sayilar[enKucukIndex];
-            sayilar[enKucukIndex] = sayilar[enBuyukIndex];
-            sayilar[enBuyukIndex] = temp;
+            //int temp = sayilar[enKucukIndex];
+            //sayilar[enKucukIndex] = sayilar[enBuyukIndex];
+            //sayilar[enBuyukIndex] = temp;
 
-            Console.WriteLine($"En büyük sayı = {enBuyuk}, En küçük sayı = {enKucuk}");
+            //Console.WriteLine($"En büyük sayı = {enBuyuk}, En küçük sayı = {enKucuk}");
             #endregion
 
             #region Sorting
 
+            for (int i = 0; i <= sayilar.Length - 1; i++)
+            {
+                for (int j = 1; j <= sayilar.Length - 1; j++)
+                {
+                    if(sayilar[j-1] > sayilar[j])
+                    {
+                        int temp = sayilar[j - 1];
+                        sayilar[j - 1] = sayilar[j];
+                        sayilar[j] = temp;
+                    }
+                }
+
+                for (int x = 0; x < sayilar.Length; x++)
+                {
+                    Console.Write($"{sayilar[x]}-");
+                }
+                Console.WriteLine("----------------------------");
+            }
+
+            // Quick sort algoritmasını kullanarak sıralama işlemini yapınız.
             #endregion
         }
     }
