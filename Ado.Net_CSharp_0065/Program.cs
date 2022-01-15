69066ea76518198dbaff6ed4313bd1d063e8f674
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-01-15 15:31:01 +0300

    Ado.Net giriş

---
+++ Ado.Net_CSharp_0065/Program.cs
@@ -0,0 +1,34 @@
+﻿using Ado.Net_CSharp_0065.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.Net_CSharp_0065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string connectionString = "Data source=DESKTOP-6VC48K2; Initial Catalog=NORTHWND; Integrated Security=True"; //Windows auth
+            string connectionString = "Data source=DESKTOP-6VC48K2; Initial Catalog=NORTHWND; User Id=northwnduser; Password=123"; //Sql server auth
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string command = "Select ProductName,UnitPrice from Products";
+            SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            List<Products> products = new List<Products>();
+            while (reader.Read())
+            {
+                var product = new Products();
+                product.ProductName = reader.GetString(0);
+                product.UnitPrice = reader.GetDecimal(1);
+                products.Add(product);
+                //Console.WriteLine($"{reader.GetString(0)} - {reader.GetDecimal(1)}");
+            }
+            sqlConnection.Close();
+        }
+    }
+}
